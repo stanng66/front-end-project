93e6b3ec4aa5bd448c999b3bce17e9d3574a6f70
Stanley Nguyen <stan.ng66@gmail.com>
2026-07-29 21:30:35 -0400

    Worked on creating a fetch and input

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -8,14 +8,51 @@
 // --------------------
 // This program demonstrates 
 // ====================
-// Priject Requirement: component 1 - show fetched JSONdata
+import { useState } from "react";
 
-// Project Requirement: component 2 - ask user input data
+export default function BookSearch() {
+    const [query, setQuery] = useState("");
+    const [books, setBooks] = useState([]);
+    const [error, setError] = useState("");
 
-function BookSearch() {
+    // Priject Requirement: component 1 - show fetched JSONdata
+    const searchBooks = async () => {
+        fetch(`https://api.bigbookapi.com/search-books?query=${query}&number=10&api-key=73f838a7148744149b5199c9bd46e28e`)
+          .then(res => {
+            if (!res.ok) throw new Error("Failed to retrieve book data");
+            return res.json();
+          })
+          .then(data => {
+            setBooks(data.books);
+            setError("");
+        })
+      .catch(err => setError(err.message));
+    }; // end of fetch 
+
     return (
         <>
-            {/* Your Component 1 + 2 content goes here */}
+          <h2>Book discovery</h2>
+
+          {/* Project Requirement: component 2 - ask user input data */}
+          <input> 
+          type="text" 
+          placeholder="Search a book"
+          value={query}
+          onCharge={(e) => setQuery(e.target.value)}
+          </input>
+
+          <button onClick={searchBooks}>Search</button>
+
+          {books.map((group, index) => (
+            <div key={index}>
+                {group.map((book) => (
+                    <div key={book.id}>
+                        <h3>{book.title}</h3>
+                        {book.image && <img src={book.image} alt={book.title} />}
+                    </div>
+                ))}
+            </div>
+          ))}
         </>
     );
 }
